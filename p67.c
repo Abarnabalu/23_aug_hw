442d7a4e64607f3ab18633abb1ee4e186b22ef45
Abarnabalu <142528239+Abarnabalu@users.noreply.github.com>
2023-08-24 15:50:13 +0530

    Update p67.c

--- p67.c
+++ p67.c
@@ -21,18 +21,22 @@ printf("the total bill amount is:%.2f ",bill);
 }
 else if((unit>=200)&&((unit<400)))
 {
-bill=unit*1.5;
+bill=(unit-199)*1.5+238.8;
 printf("the total bill amount is:%.2f ",bill);
 }
 else if((unit>=400)&&((unit<600)))
 {
-bill=((unit*1.8)+((unit*1.8)*0.15));
+bill=((unit-399)*1.8)+538.8;
 printf("the total bill amount is:%.2f ",bill);
 }
 else if(unit>600)
 {
-bill=((unit*2)+((unit*2)*0.15));
+bill=((unit-599)*2)+898.8;
 printf("the total bill amount is:%.2f ",bill);
+}
+if (bill>=400)
+{
+printf("The bill amount is %.3f",((bill*0.15)+bill));
 }
 return 0;
 }
